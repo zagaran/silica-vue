f718d3866d59ea83c706f93015aa68b5ab16e684
Noah Houghton <noah@zagaran.com>
2022-03-29 16:01:15 -0400

    update dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {createVuePlugin} from "vite-plugin-vue2";
 import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite'
 import { babel } from "@rollup/plugin-babel"
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
